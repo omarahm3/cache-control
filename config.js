728706afac5d2b217c3771ab2d3943a66724aace
Omar Ahmed <omarking05@gmail.com>
2022-05-24 22:56:01 +0200

    feat(config): add ttl default value

--- config.js
+++ config.js
@@ -2,4 +2,5 @@ module.exports = {
   mongoURI: process.env.MONGO_URI || 'mongodb://localhost:27017/cache',
   port: process.env.PORT || 3000,
   maximumCacheSize: process.env.MAXIMUM_CACHE_SIZE || 10,
+  defaultTtl: process.env.DEFAULT_TTL || 60 * 60 * 5,
 }
