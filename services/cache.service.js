8c3b07c20b58dc6b97345359dd7fc949373223eb
Omar Ahmed <omarking05@gmail.com>
2022-05-24 23:52:41 +0200

    refactor(cache-service): move cache miss logic to separate function

--- services/cache.service.js
+++ services/cache.service.js
@@ -6,14 +6,54 @@ const expiredTtl = (cache) => {
   return cache && Date.now() - cache.ttl > config.defaultTtl
 }
 
+/**
+ * This will handle creating a cache record for a new cache key
+ * @param {string} key
+ * @param {Function} randomGenerator
+ */
+const handleCacheMiss = async (key, randomGenerator) => {
+  logger.info('Cache miss')
+
+  const currentCacheSize = await Cache.countDocuments()
+  const value = randomGenerator()
+
+  // Happy scenario where cache does not exceed the maximum allowed cache size
+  if (currentCacheSize < config.maximumCacheSize) {
+    // Generate new cache record and save
+    // TTL is handled by default for new documents
+    const cache = new Cache({ key, value })
+    await cache.save()
+    return value
+  }
+
+  // Cache size is exceeded
+  // Get the very oldest cache record using `updatedAt` field
+  const oldestCache = await Cache.findOne().sort({ updatedAt: -1 })
+  // Update both its key and value
+  oldestCache.key = key
+  oldestCache.value = value
+  // TTL need to be reset again, since this we're overriding an existing record with a new data
+  oldestCache.ttl = Date.now() + config.defaultTtl
+  // Save the document
+  await oldestCache.save()
+
+  return value
+}
+
 module.exports = {
   handleGetCacheByKey: async (key, randomGenerator) => {
+    // Check if we have a cache for this key
     const existingCache = await Cache.findOne({ key })
 
+    // Check if this cache TTL is expired
     if (expiredTtl(existingCache)) {
+      // Remove this cache and continue to cache miss
       await Cache.deleteOne({ key })
-    } else if (existingCache) {
+    } 
+    // Or check if cache exists and TTL is still valid
+    else if (existingCache) {
       logger.info('Cache hit')
+      // Update the TTL since this is a hit
       existingCache.ttl = Date.now() + config.defaultTtl
       await existingCache.save()
 
@@ -23,22 +63,7 @@ module.exports = {
       }
     }
 
-    logger.info('Cache miss')
-
-    const currentCacheSize = await Cache.countDocuments()
-    const value = randomGenerator()
-
-    if (currentCacheSize >= config.maximumCacheSize) {
-      // override oldest cache
-      const oldestCache = await Cache.findOne().sort({ updatedAt: -1 })
-      oldestCache.key = key
-      oldestCache.value = value
-      oldestCache.ttl = Date.now() + config.defaultTtl
-      await oldestCache.save()
-    } else {
-      const cache = new Cache({ key, value })
-      await cache.save()
-    }
+    const value = await handleCacheMiss(key, randomGenerator)
 
     return {
       hit: false,
